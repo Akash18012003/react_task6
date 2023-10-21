85c53edc41f1f05d906a298c459b497390a01f67
Mannem-Srinivas <mannem.srinivas2021@vitstudent.ac.in>
2023-10-21 07:45:01 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Addala Akash Varma</p>
+    </footer>
+  );
+};
+
+export default Footer;
